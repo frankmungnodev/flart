ce48666b01aae5bd765163fbfaaa0f94f203dcec
frankmungnodev <frankmungnodev@gmail.com>
2025-03-26 00:54:36 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"flart/internal/commands"
+	"flart/internal/config"
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/AlecAivazis/survey/v2"
+)
+
+const (
+	cmdNewScreen     = "New Screen"
+	cmdNewModel      = "New Model"
+	cmdBuildRunner   = "Build Runner"
+	cmdWatchRunner   = "Watch Runner"
+	cmdMakeModel     = "make:model"
+	cmdMakeScreen    = "make:screen"
+	cmdBuildRunnerCL = "build:runner"
+	cmdWatchRunnerCL = "watch:runner"
 )
 
 func main() {
@@ -16,40 +29,64 @@ func main() {
 }
 
 func run() error {
-	// Handle command line arguments
 	if len(os.Args) > 2 {
-		return handleCommandLine(os.Args[1], os.Args[2])
+		return handleMakeCommand(os.Args[1], os.Args[2])
 	} else if len(os.Args) > 1 {
 		return handleBuildCommand(os.Args[1])
 	}
-
 	return handleInteractive()
 }
 
 func handleBuildCommand(command string) error {
-	switch command {
-	case "build:runner":
-		if err := commands.BuildRunner(); err != nil {
-			return err
-		}
-		fmt.Printf("Freeze build_runner done!")
-
-	default:
-		fmt.Printf("Unknown command: %s\n", command)
+	cfg, err := config.Load()
+	if err != nil {
+		return fmt.Errorf("failed to load config: %w", err)
 	}
 
+	switch command {
+	case cmdBuildRunnerCL:
+		return runBuildRunner(*cfg.ProjectDir, false)
+	case cmdWatchRunnerCL:
+		return runBuildRunner(*cfg.ProjectDir, true)
+	default:
+		return fmt.Errorf("unknown command: %s", command)
+	}
+}
+
+func runBuildRunner(projectDir string, watch bool) error {
+	args := []string{"run", "build_runner"}
+	if watch {
+		args = append(args, "watch", "-d")
+	} else {
+		args = append(args, "build", "--delete-conflicting-outputs")
+	}
+
+	cmd := exec.Command("dart", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Dir = projectDir
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to run build_runner: %w", err)
+	}
+
+	action := "Build"
+	if watch {
+		action = "Watch"
+	}
+	fmt.Printf("%s build_runner completed successfully!\n", action)
 	return nil
 }
 
-func handleCommandLine(command, name string) error {
+func handleMakeCommand(command, name string) error {
 	switch command {
-	case "make:model":
+	case cmdMakeModel:
 		if err := commands.CreateModel(name); err != nil {
 			return fmt.Errorf("failed to create model: %w", err)
 		}
 		fmt.Printf("Model %s created successfully!\n", name)
 
-	case "make:screen":
+	case cmdMakeScreen:
 		if err := commands.CreateScreen(name); err != nil {
 			return fmt.Errorf("failed to create screen: %w", err)
 		}
@@ -58,14 +95,18 @@ func handleCommandLine(command, name string) error {
 	default:
 		return fmt.Errorf("unknown command: %s", command)
 	}
-
 	return nil
 }
 
 func handleInteractive() error {
-	var choice string
-	options := []string{"New Project", "New Screen", "New Model"}
+	options := []string{
+		cmdNewScreen,
+		cmdNewModel,
+		cmdBuildRunner,
+		cmdWatchRunner,
+	}
 
+	var choice string
 	prompt := &survey.Select{
 		Message: "Choose an option:",
 		Options: options,
@@ -76,35 +117,41 @@ func handleInteractive() error {
 	}
 
 	switch choice {
-	case "New Project":
-		return fmt.Errorf("project creation not implemented yet")
+	case cmdNewScreen:
+		return handleNamePrompt("screen", commands.CreateScreen)
 
-	case "New Screen":
-		var screenName string
-		if err := survey.AskOne(&survey.Input{
-			Message: "Enter screen name:",
-		}, &screenName); err != nil {
-			return fmt.Errorf("failed to get screen name: %w", err)
-		}
+	case cmdNewModel:
+		return handleNamePrompt("model", commands.CreateModel)
 
-		if err := commands.CreateScreen(screenName); err != nil {
-			return fmt.Errorf("failed to create screen: %w", err)
+	case cmdBuildRunner:
+		cfg, err := config.Load()
+		if err != nil {
+			return fmt.Errorf("failed to load config: %w", err)
 		}
-		fmt.Printf("Screen %s created successfully!\n", screenName)
+		return runBuildRunner(*cfg.ProjectDir, false)
 
-	case "New Model":
-		var modelName string
-		if err := survey.AskOne(&survey.Input{
-			Message: "Enter model name:",
-		}, &modelName); err != nil {
-			return fmt.Errorf("failed to get model name: %w", err)
+	case cmdWatchRunner:
+		cfg, err := config.Load()
+		if err != nil {
+			return fmt.Errorf("failed to load config: %w", err)
 		}
-
-		if err := commands.CreateModel(modelName); err != nil {
-			return fmt.Errorf("failed to create model: %w", err)
-		}
-		fmt.Printf("Model %s created successfully!\n", modelName)
+		return runBuildRunner(*cfg.ProjectDir, true)
 	}
 
+	return nil
+}
+
+func handleNamePrompt(itemType string, createFn func(string) error) error {
+	var name string
+	if err := survey.AskOne(&survey.Input{
+		Message: fmt.Sprintf("Enter %s name:", itemType),
+	}, &name); err != nil {
+		return fmt.Errorf("failed to get %s name: %w", itemType, err)
+	}
+
+	if err := createFn(name); err != nil {
+		return fmt.Errorf("failed to create %s: %w", itemType, err)
+	}
+	fmt.Printf("%s %s created successfully!\n", itemType, name)
 	return nil
 }
